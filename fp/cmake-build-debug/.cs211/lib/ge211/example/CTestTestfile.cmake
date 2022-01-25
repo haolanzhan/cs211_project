d05bb1df3c23c8c27effcea4643eef47ecbd7a16
Haolan Zhan <haolanzhan@gmail.com>
2022-01-25 00:05:33 -0600

    random

--- fp/cmake-build-debug/.cs211/lib/ge211/example/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/haolanzhan/Northwestern/Spring Quarter/cs211/GitHub/CS211/fp/.cs211/lib/ge211/example
-# Build directory: /Users/haolanzhan/Northwestern/Spring Quarter/cs211/GitHub/CS211/fp/cmake-build-debug/.cs211/lib/ge211/example
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
